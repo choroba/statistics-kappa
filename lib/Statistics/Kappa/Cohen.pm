f8a326760b46387eb753fe53eac2bad81d6b9c3d
E. Choroba <choroba@matfyz.cz>
2026-09-13 23:31:17 +0200

    Initial commit create from Module::Starter

---
+++ lib/Statistics/Kappa/Cohen.pm
@@ -0,0 +1,105 @@
+package Statistics::Kappa::Cohen;
+
+use 5.008003;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Statistics::Kappa::Cohen - The great new Statistics::Kappa::Cohen!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Statistics::Kappa::Cohen;
+
+    my $foo = Statistics::Kappa::Cohen->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+E. Choroba <choroba@matfyz.cz>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-statistics-kappa-cohen at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Statistics-Kappa-Cohen>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Statistics::Kappa::Cohen
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Statistics-Kappa-Cohen>
+
+=item * GitHub issue tracker
+
+L<https://github.com/choroba/Statistics-Kappa-Cohen/issues>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Statistics-Kappa-Cohen>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2026 by E. Choroba <choroba@matfyz.cz>.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Statistics::Kappa::Cohen
