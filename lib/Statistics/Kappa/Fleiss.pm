cfa04274bed7ec719237d4e2b94d8671c5c1c033
E. Choroba <choroba@matfyz.cz>
2026-09-25 23:39:39 +0200

    Add Fleiss Kappa

---
+++ lib/Statistics/Kappa/Fleiss.pm
@@ -0,0 +1,136 @@
+package Statistics::Kappa::Fleiss;
+use 5.026;
+
+our $VERSION = '0.02';
+
+use Moo;
+use experimental qw{ signatures };
+
+use List::Util qw{ sum };
+use namespace::clean;
+
+has data           => (is => 'ro',   required => 1);
+has kappa          => (is => 'lazy', init_arg => undef);
+has proportion     => (is => 'lazy', init_arg => undef);
+has extent         => (is => 'lazy', init_arg => undef);
+has category_count => (is => 'lazy', init_arg => undef);
+has mean           => (is => 'lazy', init_arg => undef);
+has pe             => (is => 'lazy', init_arg => undef);
+
+
+sub _build_category_count($self) {
+    sum(@{ $self->data->[0] })
+}
+
+sub _build_proportion($self) {
+    my %p;
+    for my $j (0 .. $#{ $self->data->[0] }) {
+        $p{$j} = sum(map $self->data->[$_][$j], 0 .. $#{ $self->data })
+               / (@{ $self->data } * $self->category_count);
+    }
+    return \%p
+}
+
+sub _build_extent($self) {
+    my %p;
+    $p{$_} = (sum(map $_ ** 2, @{ $self->data->[$_] }) - $self->category_count)
+           / $self->category_count / ($self->category_count - 1)
+           for 0 .. $#{ $self->data };
+    return \%p
+}
+
+sub _build_mean($self) {
+    sum(values %{ $self->extent }) / @{ $self->data }
+}
+
+sub _build_pe($self) {
+    sum(map $_ ** 2, values %{ $self->proportion })
+}
+
+sub _build_kappa($self) {
+    ($self->mean - $self->pe) / (1 - $self->pe)
+}
+
+
+
+
+=head1 NAME
+
+Statistics::Kappa::Fleiss - Calculate inter-annotator agreement.
+
+=head1 VERSION
+
+Version 0.02
+
+=head1 SYNOPSIS
+
+    use Statistics::Kappa::Fleiss;
+
+    my @data = ([0, 0, 0, 0, 14],
+                [0, 2, 6, 4, 2],
+                [0, 0, 3, 5, 6]);
+    my $fk = 'Statistics::Kappa::Fleiss'->new(data => \@data);
+    my $kappa = $fk->kappa;
+
+=head1 METHODS
+
+=head2 new
+
+The constructor. It takes a named argument C<data> which should contain an
+array reference. Each element of the array represents a rated item by another
+anonymous array, which on position C<j> contains the number of raters that
+assigned the category C<j> to the item. Therefore, the format of the data is
+I<different> to other L<Statistics::Kappa> modules.
+
+=head1 AUTHOR
+
+E. Choroba <choroba@matfyz.cz>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-statistics-kappa-cohen at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Statistics-Kappa-Cohen>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Statistics::Kappa::Fleiss
+
+
+You can also look for information at:
+
+=over 4
+
+=item * GitHub issue tracker (report bugs here)
+
+L<https://github.com/choroba/Statistics-Kappa>
+
+=item * RT: CPAN's request tracker (or here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Statistics-Kappa>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Statistics-Kappa>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2026 by E. Choroba <choroba@matfyz.cz>.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+__PACKAGE__
